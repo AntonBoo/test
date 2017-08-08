ae9aeb54fe7efc6223bbbf2acfa1c0102c0ded3c
AntonBoo <anthonyehds@gmail.com>
2017-08-08 16:15:35 +0200

    "second commit"

--- src/test/App.java
+++ src/test/App.java
@@ -5,7 +5,7 @@ public class App {
 	public static void main(String[] args) {
 
 		String str = "this is a test";
-		System.out.println(str);
+		System.out.println(str + " and second change");
 	}
 
 }
